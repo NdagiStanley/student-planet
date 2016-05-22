1ee3d26a2e485a80f8ef69e379b6ebb2f9984320
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-22 18:01:38 +0300

    Update bio for student

--- resources/views/student/student_profile.blade.php
+++ resources/views/student/student_profile.blade.php
@@ -19,9 +19,7 @@
             <div class="col-md-4 list-group">
                 <h4 class="list-group-item-heading">Bio</h4>
                 <p class="list-group-item-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    <br>
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+                    a student that is looking to get experience in the workforce through the opportunities that will be provided in this platform, get to build my skills and learn from others in my field of expertise.</p>
                 </a>
             </div>
         </div>
