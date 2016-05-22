9e7199421ef5159882542508eeba7ea0ed4212eb
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-22 17:14:30 +0300

    Edit student profile page

--- resources/views/student/student_profile.blade.php
+++ resources/views/student/student_profile.blade.php
@@ -16,7 +16,13 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 list-group">
+                <h4 class="list-group-item-heading">Bio</h4>
+                <p class="list-group-item-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    <br>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
+                </a>
             </div>
         </div>
         <div class="col-md-12">
